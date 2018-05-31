381f42418fa2870dc4c9edf684992aa12ca8f4cc
Mihail Tratsiuk <mihail.tratsiuk@gmail.com>
2018-05-31 19:50:36 +0300

    Fix isFreeVarOf
    Abstraction of type \x."s"x  should not be reduced

--- app/Main.hs
+++ app/Main.hs
@@ -17,14 +17,16 @@ testEtaReduce =
     , etaReduce not_reducable_1 == not_reducable_1
     , etaReduce not_reducable_2 == not_reducable_2
     , etaReduce not_reducable_3 == not_reducable_3
+    , etaReduce not_reducable_4 == not_reducable_4
     ]
   where
     not_reducable_1 = Abs "x" (App (Abs "y" (Term "x")) (Term "x"))
     not_reducable_2 = Abs "x" (App (Abs "y" (Abs "z" (Term "x"))) (Term "x"))
     not_reducable_3 = Abs "x" (App (App l_const (Term "x")) (Term "x"))
+    not_reducable_4 = Abs "x" (App (Lit "s") (Term "x"))
 
 isFreeVarOf :: Identifier -> Expr -> Bool
-isFreeVarOf var (Lit s) = False
+isFreeVarOf var (Lit s) = True
 isFreeVarOf var (Term t) = var == t
 isFreeVarOf var (Abs ident body) = var /= ident && isFreeVarOf var body
 isFreeVarOf var (App t u) = isFreeVarOf var t || isFreeVarOf var u
